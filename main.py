9ce47c770abda0d426b0287e2a445deb4c6d0197
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-01-28 18:53:54 +0200

    3 and 4 tasks add

--- main.py
+++ main.py
@@ -9,4 +9,16 @@ print(f'Atsitiktinis nuo 1 iki 10: {atsitiktinis_skaicius}')
 #     if skaicius == random.randint(1,10):
 #         break
 
+# Example spec functions
+from random import randint, choice
 
+random_number = randint(1,10)
+print(random_number)
+
+random_month = choice(['sausis','vasaris','kovas'])
+print(random_month)
+
+# Example alias
+import random as rn
+atsitiktinis_skaicius = rn.randint(1,10)
+print(f'Atsitiktinis nuo 1 iki 10: {atsitiktinis_skaicius}')
--- tasks.py
+++ tasks.py
@@ -3,6 +3,24 @@ import random
 
 random_number = random.randint(1,100)
 print(f'Random number from 1 to 100 is: {random_number}')
+
 #  2 Task
 random_number1 = random.uniform(1,50)
 print(f'Random uniform number from 1 to 50 is: {random_number1}')
+
+#  3 Task
+# 1.
+from random import randint, choice
+
+random_number = randint(1,10)
+print(random_number)
+
+ # 2.
+random_fruit = choice(['obuolys','bananas','kriaušė',
+'vyšnia'])
+print(random_fruit)
+
+# 4 Task
+import datetime as dt
+current_time = dt.datetime.now()
+print(current_time)

e685ebaa5db0b590a86558cdc850ce72ff973a2b
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-01-28 20:36:01 +0200

    7-10 tasks add

---
+++ 7-10-task/task-7-8-9-10.py
@@ -0,0 +1,12 @@
+# 7, 8
+import matematika
+print(matematika.sudetis(10, 20))
+print(matematika.daugyba(5, 4))
+# 9
+from matematika import sudetis,daugyba
+print(sudetis(8,3))
+print(daugyba(8,3))
+# 10
+import matematika as m
+print(m.sudetis(12,18))
+print(m.daugyba(7,6))

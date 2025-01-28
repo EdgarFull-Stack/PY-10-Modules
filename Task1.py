f24888901e274ac3b8aab48343f9afc0b3dcc132
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-01-28 18:31:37 +0200

    1 task add

---
+++ Task1.py
@@ -0,0 +1,8 @@
+# 1 Task
+import random
+
+random_number = random.randint(1,100)
+print(f'Random number from 1 to 100 is: {random_number}')
+#  2 Task
+random_number1 = random.uniform(1,50)
+print(f'Random uniform number from 1 to 50 is: {random_number1}')
---
+++ main.py
@@ -0,0 +1,12 @@
+
+import random
+# Example
+atsitiktinis_skaicius = random.randint(1,10)
+print(f'Atsitiktinis nuo 1 iki 10: {atsitiktinis_skaicius}')
+
+# while True:
+#     skaicius = int(input('Ar galite atspeti skaiciu? (veskite kol neatspesite) :'))
+#     if skaicius == random.randint(1,10):
+#         break
+
+
